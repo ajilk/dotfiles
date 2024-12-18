62633f1d9690fc6d025a4756f80b66ec186a31de
Azimjon Ilkhomov <ajilkhomov@gmail.com>
2024-12-18 12:25:56 -0500

    Update padding

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -4,4 +4,10 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%m/%d %I:%M %p')"
+sketchybar --set "$NAME" label="$(date '+%m/%d %I:%M %p')" \
+           --set "$NAME" icon.padding_left=0 \
+           --set "$NAME" icon.padding_right=0 \
+           --set "$NAME" label.padding_left=0 \
+           --set "$NAME" label.padding_right=0 \
+           --set "$NAME" background.padding_left=0 \
+           --set "$NAME" background.padding_right=0
